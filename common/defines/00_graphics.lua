fbc0013cd061fcadee9193bf14dea57e9e7ad4f5
antmrt <64744962+antmrt@users.noreply.github.com>
2022-11-03 19:13:53 +0100

    missing define

--- common/defines/00_graphics.lua
+++ common/defines/00_graphics.lua
@@ -785,6 +785,7 @@ NDefines_Graphics = {
         COUNTRY_FLAG_TEX_HEIGHT = 52,
         COUNTRY_FLAG_MEDIUM_TEX_WIDTH = 41,
         COUNTRY_FLAG_MEDIUM_TEX_HEIGHT = 26,
+        COUNTRY_FLAG_MEDIUM_TEX_MAX_SIZE = 1024,
         COUNTRY_FLAG_SMALL_TEX_WIDTH = 10,
         COUNTRY_FLAG_SMALL_TEX_HEIGHT = 7,
         COUNTRY_FLAG_TEX_MAX_SIZE = 256, -- Tweak dependly on amount of countries. Must be power of 2. No more then 2048.
